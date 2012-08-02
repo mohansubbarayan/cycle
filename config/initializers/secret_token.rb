61a0a8a787df406364f72bfbe05953d795f87d2e
unknown <maisa@maisa-PC.(none)>
2012-08-02 16:10:05 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cycle::Application.config.secret_token = '5ff012f9dd1a36046e28d749b7f1d5ee90a587954d67fb945a7f0bcbf5310018c002e370038f17924bc2e574fe7d4c557fd818e4747eb7676c72c05e1c982940'
